4657c2ee06b084bacaec9ab38595d9d1b0c87c31
1210056515@qq.com <1210056515@qq.com>
2020-11-25 09:13:13 +0800

    init

---
+++ douban/items.py
@@ -0,0 +1,26 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoubanItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # pass
+    # 序号
+    serial_number = scrapy.Field()
+    # 电影名字
+    movie_name = scrapy.Field()
+    # 电影介绍
+    introduce = scrapy.Field()
+    # 星级
+    star = scrapy.Field()
+    # 电影描述
+    evaluate = scrapy.Field()
+    describe = scrapy.Field()
+    #图片
+    image = scrapy.Field()
+
---
+++ douban/pipelines.py
@@ -0,0 +1,48 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+from douban.settings import MYSQL_DBNAME,MYSQL_HOST,MYSQL_PASSWD,MYSQL_PORT,MYSQL_USER
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class DoubanPipeline:
+    #打开数据库
+    def open_spider(self, spider):
+        db = spider.settings.get('MYSQL_DBNAME')
+        host = spider.settings.get('MYSQL_HOST')
+        port = spider.settings.get('MYSQL_PORT')
+        user = spider.settings.get('MYSQL_USER')
+        passwd = spider.settings.get('MYSQL_PASSWD')
+        self.db_conn = pymysql.connect(host=host,port=port,db=db,user=user,passwd=passwd)
+        self.db_cur = self.db_conn.cursor()
+
+    #关闭数据库
+    def close_spider(self,spider):
+        self.db_conn.commit()
+        self.db_conn.close()
+
+    #对数据库进行处理
+    def process_item(self, item, spider):
+        self.insert_db(item)
+        return item
+
+    #插入数据
+    def insert_db(self,item):
+        values = (
+            item['serial_number'],
+            item['movie_name'],
+            item['introduce'],
+            item['star'],
+            item['evaluate'],
+            item['describe'],
+            item['image']
+        )
+        print("hello")
+        print(values)
+
+        sql = 'INSERT INTO books VALUES(NULL,%s,%s,%s,%s,%s,%s,%s)'
+        self.db_cur.execute(sql, values)
