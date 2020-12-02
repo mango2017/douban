5d7974b41cd545a646fda39b4a30931489748bb2
1210056515@qq.com <1210056515@qq.com>
2020-12-02 15:49:34 +0800

    1

--- webauto.py
+++ webauto.py
@@ -26,15 +26,69 @@ from time import sleep
 #     print(span.text)
 
 
+# wd = webdriver.Chrome()
+# # 设置最大等待时长为 10秒
+# wd.implicitly_wait(10)
+# wd.get("https://www.baidu.com")
+# element = wd.find_element_by_id('kw')
+# element.send_keys('白月黑雨\n')
+# # sleep(2)
+# element = wd.find_element_by_id("1")
+# print(element.get_attribute("srcid"))
+# print(element.get_attribute('outerHTML'))
+# print(element.get_attribute('innerHTML'))
+#
+# wd.quit()
+
+
+
+
 wd = webdriver.Chrome()
 # 设置最大等待时长为 10秒
-wd.implicitly_wait(10)
-wd.get("https://www.baidu.com")
-element = wd.find_element_by_id('kw')
-element.send_keys('白月黑雨\n')
-# sleep(2)
-element = wd.find_element_by_id("1")
-print(element.get_attribute("srcid"))
-print(element.get_attribute('outerHTML'))
-print(element.get_attribute('innerHTML'))
-wd.quit()
+
+# wd.get("http://cdn1.python3.vip/files/selenium/sample1.html")
+# element = wd.find_element_by_css_selector(".animal")
+# print(element.get_attribute("outerHTML"))
+
+# elements = wd.find_elements_by_css_selector("span")
+# for element in elements:
+#     print(element.get_attribute("outerHTML"))
+
+# element = wd.find_element_by_css_selector("#searchtext")
+# print(element.get_attribute('outerHTML'))
+# 
+# wd.quit()
+
+
+# wd.get("http://cdn1.python3.vip/files/selenium/sample1.html")
+# # elements = wd.find_elements_by_css_selector("#container > div ")
+# elements = wd.find_elements_by_css_selector("#container  span ")
+# for element in elements:
+#     print(element.get_attribute("outerHTML"))
+# wd.quit()
+
+
+# wd.get("http://cdn1.python3.vip/files/selenium/sample2.html")
+# wd.switch_to.frame("frame1")
+# elements = wd.find_elements_by_css_selector(".plant")
+# for element in elements:
+#     print(element.get_attribute("outerHTML"))
+# wd.switch_to.default_content()
+# wd.find_element_by_id('outerbutton').click()
+# wd.quit()
+
+
+wd.get('http://cdn1.python3.vip/files/selenium/sample3.html')
+link = wd.find_element_by_tag_name("a")
+link.click()
+mainWindow = wd.current_window_handle
+for handle in wd.window_handles:
+    # 先切换到该窗口
+    wd.switch_to.window(handle)
+    # 得到该窗口的标题栏字符串，判断是不是我们要操作的那个窗口
+    if 'Bing' in wd.title:
+        # 如果是，那么这时候WebDriver对象就是对应的该该窗口，正好，跳出循环，
+        break
+print(wd.title)
+wd.find_element_by_id("sb_form_q").send_keys("白月黑雨")
+wd.switch_to.window(mainWindow)
