3a17b8434fbec6dd11122fa09bfef045f7341bdf
1210056515@qq.com <1210056515@qq.com>
2020-12-10 15:01:03 +0800

    1

---
+++ python/mulu.py
@@ -0,0 +1,21 @@
+import os
+
+# os.makedirs("tmp/python/fileop",exist_ok=True)
+
+
+# os.remove('sdf.py')
+
+# import shutil
+
+# shutil.rmtree('tmp')
+
+
+# from shutil import  copyfile
+#
+# copyfile('F:/a.php','G:/a.php')
+
+# os.rename('./abc.py','./bcd.py')
+
+
+path = ''
+os.path.basename(path)
